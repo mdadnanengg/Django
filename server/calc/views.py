ec5599fbc9157897615df19d433025965ae9ba7f
Mohammad Adnan <mdadnanengg@gmail.com>
2024-02-17 18:39:24 +0530

    change get method to post

--- server/calc/views.py
+++ server/calc/views.py
@@ -7,8 +7,8 @@ def home (req):
     return render(req, 'home.html', {'name': 'Adnan'})
 
 def add(req):
-    val1 = int(req.GET['num1'])
-    val2 = int(req.GET['num2'])
+    val1 = int(req.POST['num1'])
+    val2 = int(req.POST['num2'])
     res = val1 + val2
 
     return render(req, 'result.html', {'result': res})
